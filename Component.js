ad2dbfaa030062da93cbd5b2f43b8e05660d07bd
yash <124248317+yaswanthbodd@users.noreply.github.com>
2024-06-21 16:57:08 +0530

    Structure of the Component

---
+++ Component.js
@@ -0,0 +1,23 @@
+import React from 'react'
+
+const Component=()=>{
+    return(
+        <div>
+            <User />
+            <User />
+        </div>
+    )
+}
+
+//User Component
+const User = () =>{
+    return(
+        <div>
+            <h1>Yaswanth</h1>
+            <h2>21</h2>
+            <h2>yaswanth09@gmail.com</h2>
+        </div>
+    )
+}
+
+export default Component
